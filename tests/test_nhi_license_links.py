143fda0b1cb976a4019cf4ae6753b923ad762c8e
LCCtaiwan <epyonlee2001@gmail.com>
2026-09-14 18:54:59 +0800

    fix(nhi): use full license source and preserve inactive and special identifiers

--- tests/test_nhi_license_links.py
+++ tests/test_nhi_license_links.py
@@ -22,7 +22,7 @@ class LicenseLinksTest(unittest.TestCase):
     def test_customs_serial_must_agree(self):
         self.assertEqual(m.customs_license_id(source()), '01048092')
         self.assertEqual(m.customs_license_id(source(customs='DHY00104809300')), '')
-        self.assertEqual(m.customs_license_id(source(customs='DHYS0104809200')), '')
+        self.assertEqual(m.customs_license_id(source(customs='DHYS0104809200')), '01048092')
         self.assertEqual(m.customs_license_id(source(customs='')), '')
 
     def test_missing_cancelled_and_ambiguous_never_autolink(self):
@@ -43,3 +43,19 @@ class LicenseLinksTest(unittest.TestCase):
             m.link_nhi_licenses(records,[{'license':source()['許可證字號']}],Path('unused'))
         self.assertEqual(records[0]['licenses'],[])
         self.assertEqual(records[0]['licenseStatus'],'missing_official_id')
+
+    def test_radioactive_license_serial_keeps_r(self):
+        self.assertEqual(m.customs_license_id(source('衛部藥輸字第R00091號','DHA052R0009101')), '52R00091')
+        self.assertEqual(m.official_license_id('https://lmspiq.fda.gov.tw/web/DRPIQ/DRPIQ1000Result?licId=52R00091'), '52R00091')
+
+    def test_missing_customs_requires_unambiguous_observed_type(self):
+        rows=[source('衛署藥製字第000001號','DHY00100000100'),source('衛署藥製字第000002號','DHY00100000200'),source(customs='')]
+        records=[{'drugUrl':'https://lmspiq.fda.gov.tw/web/DRPIQ/DRPIQ1000Result?licId=01048092'}]
+        with patch.object(m,'read_tfda_json',return_value=rows):
+            m.link_nhi_licenses(records,[{'license':source()['許可證字號']}],Path('unused'))
+        self.assertEqual(records[0]['licenseMethod'],'license_type_normalization')
+        self.assertEqual(records[0]['licenseStatus'],'matched')
+        rows[1]['通關簽審文件編號']='DHY00300000200'
+        with patch.object(m,'read_tfda_json',return_value=rows):
+            m.link_nhi_licenses(records,[{'license':source()['許可證字號']}],Path('unused'))
+        self.assertEqual(records[0]['licenseStatus'],'unresolved')
